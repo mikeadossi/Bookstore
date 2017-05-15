805c7a66e7747577b95eb1e78de002218e0aefeb
mikeadossi <mikeadossi@gmail.com>
2017-05-15 12:28:38 -0700

    frontend set up

---
+++ frontend/src/components/Home/Home.js
@@ -0,0 +1,13 @@
+import './home.css'
+import React from 'react';
+
+export default class Home extends React.Component{
+
+  render(){
+    return(
+      <div className="home_container">
+        <p>Home!</p>
+      </div>
+    );
+  };
+}
