28a801b0024420be09bc651a14d4a989bea74933
mikeadossi <mikeadossi@gmail.com>
2017-05-16 00:47:37 -0700

    create Admin page & write styling

---
+++ src/components/Admin/Admin.js
@@ -0,0 +1,15 @@
+import './admin.css'
+import React from 'react';
+
+export default class Admin extends React.Component{
+
+  render(){
+    return(
+      <div className="admin_comp_container">
+        <div className="admin_container">
+          <div className="admin_title">Admin</div>
+        </div>
+      </div>
+    );
+  };
+}
