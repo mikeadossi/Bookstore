a8a867333f34547331e96861704114d7a8c856b5
mikeadossi <mikeadossi@gmail.com>
2017-05-16 09:36:38 -0700

    create footer links

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,10 +1,17 @@
 import './footer.css'
 import React from 'react';
+import { Link }  from 'react-router-dom';
 
 export default class Footer extends React.Component{
   render(){
     return(
       <div className="footer_container">
+        <div className="footer_sub_div">
+          <Link to="/about" className="footer_link">about</Link>
+          <Link to="/home" className="footer_link">home</Link>
+          <Link to="/account" className="footer_link">account</Link>
+          <Link to="/inventory" className="footer_link">inventory</Link>
+        </div>
         <p className="footer_text">Made with </p><span className="footer_heart fa fa-heart"></span><p className="footer_text"> in Oakland</p>
       </div>
     );
