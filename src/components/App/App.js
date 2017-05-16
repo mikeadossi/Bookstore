3defcd99973f4bb6e259e1f5d3c41af3434386e5
mikeadossi <mikeadossi@gmail.com>
2017-05-16 00:25:53 -0700

    misc edits

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -5,13 +5,14 @@ import Home from 'components/Home/Home';
 import Access from 'components/Access/Access';
 import Nav from 'components/Nav/Nav';
 import Footer from 'components/Footer/Footer';
+import Listing from 'components/Listing/Listing';
 
 export default class App extends React.Component{
   render(){
     return(
       <div className="app_container">
         <Nav />
-        <Access />
+        <Listing />
         <Footer />
       </div>
     );
