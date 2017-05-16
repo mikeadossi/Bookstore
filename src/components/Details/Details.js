fa8b73ad7de893da0c62f80be5ecdb21e4fa03ee
mikeadossi <mikeadossi@gmail.com>
2017-05-16 00:40:54 -0700

    create Details page & write styling

---
+++ src/components/Details/Details.js
@@ -0,0 +1,15 @@
+import './details.css'
+import React from 'react';
+
+export default class Details extends React.Component{
+
+  render(){
+    return(
+      <div className="details_comp_container">
+        <div className="details_container">
+          <div className="details_title">Details</div>
+        </div>
+      </div>
+    );
+  };
+}
