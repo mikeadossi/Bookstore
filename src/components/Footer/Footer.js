4fa438ac460e9d77e85de4c61b8a3df174135d9e
mikeadossi <mikeadossi@gmail.com>
2017-05-15 17:24:27 -0700

    create footer component

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,12 @@
+import './footer.css'
+import React from 'react';
+
+export default class Footer extends React.Component{
+  render(){
+    return(
+      <div className="footer_container">
+        <div className="footer_text">Made in Oakland</div>
+      </div>
+    );
+  };
+}
