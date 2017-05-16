090f0ff192a7c6a73dc5cdb55e712c3728d67304
mikeadossi <mikeadossi@gmail.com>
2017-05-16 06:53:40 -0700

    fully responsive nav bar

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -4,19 +4,52 @@ import { Link }  from 'react-router-dom';
 
 export default class Nav extends React.Component{
 
+  constructor(props){
+    super(props)
+    this.state = {
+      menu_styling : 'nav_top_hamburger fa fa-bars fa-lg',
+      menu_wide_styling : 'nav_hamburger fa fa-bars fa-lg',
+      menu_open : 'nav_link_bottom display_none',
+    }
+
+    this.toggleMenu = this.toggleMenu.bind(this);
+  }
+
+  toggleMenu(){
+    console.log('click!')
+    let menu = this.state.menu_styling;
+    let menu_wide = this.state.menu_wide_styling;
+
+    if(menu === 'nav_top_hamburger fa fa-bars fa-lg'){
+      this.setState({
+        menu_styling : 'nav_top_hamburger fa fa-close fa-lg',
+        menu_wide_styling : 'nav_hamburger fa fa-close fa-lg',
+        menu_open : 'nav_link_bottom display_block'
+      })
+      return
+    } else {
+      this.setState({
+        menu_styling : 'nav_top_hamburger fa fa-bars fa-lg',
+        menu_wide_styling : 'nav_hamburger fa fa-bars fa-lg',
+        menu_open : 'nav_link_bottom display_none'
+      })
+      return
+    }
+  }
+
   render(){
     return(
       <div className="nav_container">
         <div className="nav_top_container">
           <Link to="/home" className="nav_title">Bookstore</Link>
-        <div className="nav_hamburger fa fa-bars fa-lg" aria-hidden="true"></div>
+          <div className={this.state.menu_wide_styling} aria-hidden="true" onClick={this.toggleMenu}></div>
           <div className="nav_top_search">
             <input className="nav_input" placeholder="search" />
             <button className="nav_search_btn">submit</button>
           </div>
-          <div className="nav_top_hamburger fa fa-bars fa-lg" aria-hidden="true"></div>
+          <div className={this.state.menu_styling} aria-hidden="true" onClick={this.toggleMenu}></div>
           <div className="nav_wide_links_container">
-            <Link to="/access" className="nav_wide_links">log in</Link>
+            <Link to="/access" className="nav_wide_links nav_wide_logIn">log in</Link>
             <Link to="/access" className="nav_wide_links">sign up</Link>
             <Link to="/home" className="nav_wide_links">home</Link>
             <Link to="/account" className="nav_wide_links">account</Link>
@@ -27,11 +60,11 @@ export default class Nav extends React.Component{
             <input className="nav_input" placeholder="search" />
             <button className="nav_search_btn">submit</button>
           </div>
-          <div className="nav_link_bottom">
-            <Link to="/access" className="nav_links">log in</Link>
-            <Link to="/access" className="nav_links">sign up</Link>
-            <Link to="/home" className="nav_links">home</Link>
-            <Link to="/account" className="nav_links">account</Link>
+          <div className={this.state.menu_open}>
+            <Link to="/access"><div className="nav_links">log in</div></Link>
+            <Link to="/access"><div className="nav_links">sign up</div></Link>
+            <Link to="/home"><div className="nav_links">home</div></Link>
+            <Link to="/account"><div className="nav_links">account</div></Link>
           </div>
         </div>
       </div>
