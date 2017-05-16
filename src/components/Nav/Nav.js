5bd53cb18758731dd450687a12d069d331bbc12f
mikeadossi <mikeadossi@gmail.com>
2017-05-15 17:23:25 -0700

    create Nav component

---
+++ src/components/Nav/Nav.js
@@ -0,0 +1,13 @@
+import './nav.css'
+import React from 'react';
+
+export default class Nav extends React.Component{
+
+  render(){
+    return(
+      <div className="nav_container">
+        <div className="nav_title">Bookstore</div>
+      </div>
+    );
+  };
+}
